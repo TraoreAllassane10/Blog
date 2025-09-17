7b5bb2585e511fd0990192b7d38c7d305bf28d96
Traore Allassane <traoreallassane2255@gmail.com>
2025-09-17 15:53:10 +0000

    Suppression d'un commentaire

--- show.php
+++ show.php
@@ -13,6 +13,11 @@ if (isset($_GET['id'])) {
     $post = $onePost->fetch(PDO::FETCH_OBJ);
 }
 
+$comments = $conn->query("SELECT * FROM comments WHERE post_id = $id");
+$comments->execute();
+
+$comment = $comments->fetchAll(PDO::FETCH_OBJ);
+
 ?>
 
 <div class="card mt-3">
@@ -26,11 +31,11 @@ if (isset($_GET['id'])) {
     <form method="POST" id="comment_data">
 
         <div class="form-floating">
-            <input name="username" type="hidden" value="<?php echo $_SESSION["username"]; ?>" class="form-control" id="floatingInput" >
+            <input name="username" type="hidden" value="<?php echo $_SESSION["username"]; ?>" class="form-control" id="comment">
         </div>
 
         <div class="form-floating">
-            <input name="post_id" type="hidden" value="<?php echo $post->id; ?>" class="form-control" id="floatingInput">
+            <input name="post_id" type="hidden" value="<?php echo $post->id; ?>" class="form-control" id="post_id">
         </div>
 
         <div class="form-floating mt-3">
@@ -42,7 +47,20 @@ if (isset($_GET['id'])) {
 
 
     </form>
+
+    <div id="msg" class="nothing mt-3"></div>
 </div>
+
+<?php foreach ($comment as $singleComment) : ?>
+    <div class="card mt-5">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $singleComment->username; ?></h5>
+            <p class="card-text"><?php echo $singleComment->comment; ?></p>
+
+             <button id="delete-btn" value="<?php echo $singleComment->id; ?>" class="btn btn-danger mt-3" >Delete</button>
+        </div>
+    </div>
+<?php endforeach; ?>
 
 <?php require "includes/footer.php"; ?>
 
@@ -61,10 +79,47 @@ if (isset($_GET['id'])) {
                 data: formdata,
 
                 success: function() {
-                    // $("#comment").text(null);
+                    $("#comment").val(null);
+                    $("#username").val(null);
+                    $("#post_id").val(null);
+
+                    $("#msg").html("Added Successfully").toggleClass("alert alert-success bg-success text-white");
+
+                    fetch();
                 }
             })
 
         });
+
+
+        $("#delete-btn").on("click", function(e) {
+            // alert("form submitted");
+            e.preventDefault();
+
+            let id = $(this).val();
+
+            $.ajax({
+                type: "POST",
+                url: "delete-comments.php",
+                data: {
+                    delete : "delete",
+                    id: id
+                },
+
+                success: function() {
+        
+                    $("#msg").html("Deleted Successfully").toggleClass("alert alert-danger bg-danger text-white");
+
+                    fetch();
+                }
+            })
+
+        });
+
+        function fetch() {
+            setInterval(() => {
+                $("body").load("show.php?id=<?php echo $_GET['id']; ?>");
+            }, 3000);
+        }
     });
 </script>
